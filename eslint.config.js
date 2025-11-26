f15d3bf2a601c267ab8a00b9ad340c34063a2a5e
Francis Padero <francis.padero@1.ustp.edu.ph>
2025-11-27 02:43:35 +0800

    Dependencies on the formatting updated cause FUCKING PRETTIER ESLINT IS BUGGY AS HELL

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,10 @@ import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
+// So far this is the correct way to setup react with eslint in flat config
+// Prettier-eslint is buggy as hell and doesn't support latest eslint@9 versions
+// Do not put prettier on the extends array, it breaks react rules. It doesn't show the errors at all.
+
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs,jsx}"],
@@ -12,6 +16,14 @@ export default defineConfig([
     rules: {
       "react/react-in-jsx-scope": "off",
       "react/jsx-uses-react": "off",
+    },
+  },
+    // Add this object for react settings
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
   },
   pluginReact.configs.flat.recommended,
