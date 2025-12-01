00da07ab4961f6627be6cbd5dc4f7d8303132335
Francis Padero <francis.padero@1.ustp.edu.ph>
2025-12-01 21:28:06 +0800

    Fix the double login path issue after logout
    - When loggin out then immediately go to login will sometimes not work and always redirect the users to landing page.

--- src/backend/hooks/auth/authLogout.js
+++ src/backend/hooks/auth/authLogout.js
@@ -1,14 +1,13 @@
 import { supabase } from "../../supabase";
 import { useQueryClient } from "@tanstack/react-query";
 import { useAuth } from "../../context/AuthProvider";
-import { useNavigate } from "react-router";
+// import { useNavigate } from "react-router";
 import { useMutation } from "@tanstack/react-query";
-
 
 export const useLogout = () => {
   const queryClient = useQueryClient();
   const { setSession, setUser, setEvent, setRecoveryMode } = useAuth();
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   const logout = async () => {
     // 1. Clear context immediately
@@ -24,16 +23,16 @@ export const useLogout = () => {
     // 3. Sign out from Supabase
     const { error } = await supabase.auth.signOut({ scope: "local" });
     if (error) throw error;
-  }
+  };
 
   return useMutation({
     mutationFn: logout,
     onSuccess: () => {
       sessionStorage.removeItem("recovery_mode");
-      navigate("/", { replace: true });
+      // navigate("/login", { replace: true });
     },
     onError: (err) => {
       console.error("Logout error:", err.message);
-    }
+    },
   });
 };
